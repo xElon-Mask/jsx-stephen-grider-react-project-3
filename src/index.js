64402499a98575e5eeee792c61c1dc5cc608933d
xElon-Mask <xelonmask@protonmail.com>
2022-08-05 17:26:54 +0200

    get a reference to the div with ID root

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // 2 - Get a reference to the div with ID root
+const el = document.getElementById('root');
 
 // 3 - Tell React to take control of that element
 
